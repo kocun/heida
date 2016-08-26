5d827ff7104db37ae89d97817eeaed7d03f0f5ff
Berkant Eskicioglu <berkant.eskicioglu@gmail.com>
2016-08-26 22:12:45 +0300

    Frontend typo and dashboard summary area ui fix

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -46,22 +46,14 @@ angular
               $ocLazyLoad.load({
                 name: 'ngAnimate',
                 files: ['bower_components/angular-animate/angular-animate.js']
-              })
+              }),
             $ocLazyLoad.load({
               name: 'ngCookies',
               files: ['bower_components/angular-cookies/angular-cookies.js']
-            })
+            }),
             $ocLazyLoad.load({
               name: 'ngResource',
               files: ['bower_components/angular-resource/angular-resource.js']
-            })
-            $ocLazyLoad.load({
-              name: 'ngSanitize',
-              files: ['bower_components/angular-sanitize/angular-sanitize.js']
-            })
-            $ocLazyLoad.load({
-              name: 'ngTouch',
-              files: ['bower_components/angular-touch/angular-touch.js']
             })
           }
         }
