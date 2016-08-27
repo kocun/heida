d3c6fa3074206df0dbf4c9490c869b43c682e593
Berkant Eskicioglu <berkant.eskicioglu@gmail.com>
2016-08-27 18:52:07 +0300

    Frontend fix users_edit page and fix departments page

--- assets/scripts/controllers/departments.js
+++ assets/scripts/controllers/departments.js
@@ -19,6 +19,14 @@ angular.module('heidaApp')
         reload: true,
         inherit: false
       });
+    };
+
+    $scope.save = function(department) {
+      $scope.departments.post(department);
+      $state.go('dashboard.departments', $stateParams, {
+        reload: true,
+        inherit: false
+      });
     }
   }).controller('DepartmentEditCtrl', function($scope, $position, Restangular, $stateParams, $state) {
     Restangular.one('/api/department', $stateParams.id).get().then(function(department) {
@@ -26,18 +34,6 @@ angular.module('heidaApp')
     });
     $scope.update = function() {
       $scope.department.save();
-      $state.go('dashboard.departments', $stateParams, {
-        reload: true,
-        inherit: false
-      });
-    }
-  }).controller('DepartmentNewCtrl', function($scope, $position, Restangular, $stateParams, $state) {
-    Restangular.all('/api/department').getList().then(function(departments) {
-      $scope.departments = departments;
-    });
-    $scope.save = function(department) {
-      console.log(department);
-      $scope.departments.post(department);
       $state.go('dashboard.departments', $stateParams, {
         reload: true,
         inherit: false
