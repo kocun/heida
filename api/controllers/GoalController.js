9a548c6f3d39fa9b5b4b7e9b35d0e999410c65cf
Adil Atılgan <aatilgan@ku.edu.tr>
2016-08-28 21:40:30 +0300

    test

---
+++ api/controllers/GoalController.js
@@ -0,0 +1,11 @@
+/**
+ * GoalController
+ *
+ * @description :: Server-side logic for managing goals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
