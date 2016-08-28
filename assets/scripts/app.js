aaecbc72133ac9d59a3211a9cf380e8acaadcd00
Berkant Eskicioglu <berkant.eskicioglu@gmail.com>
2016-08-28 13:48:02 +0300

    Frontend fix quetion page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -467,25 +467,6 @@ angular
           }
         }
       })
-      .state('dashboard.questions_new', {
-        controller: 'QuestionNewCtrl',
-        templateUrl: 'views/pages/questions_new.html',
-        url: '/questions/new',
-        resolve: {
-          loadMyFiles: function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name: 'heidaApp',
-              files: [
-                'scripts/controllers/questions.js',
-                'scripts/directives/timeline/timeline.js',
-                'scripts/directives/notifications/notifications.js',
-                'scripts/directives/chat/chat.js',
-                'scripts/directives/dashboard/stats/stats.js'
-              ]
-            })
-          }
-        }
-      })
       .state('dashboard.questions_edit', {
         controller: 'QuestionEditCtrl',
         templateUrl: 'views/pages/questions_edit.html',
