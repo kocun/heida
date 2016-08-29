c618a27a208396e7ccb7994fdf113f2e8f93e26d
Zafer Taskent <zafer.taskent@gmail.com>
2016-08-29 09:22:38 -0400

    Goal Service implemented

---
+++ api/controllers/GoalController.js
@@ -0,0 +1,11 @@
+/**
+ * GoalController
+ *
+ * @description :: Server-side logic for managing Goals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
