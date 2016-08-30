38bfda85b6465088b318994650e7425eecf91d78
Berkant Eskicioglu <berkanteskicioglu@gmail.com>
2016-08-30 22:28:25 +0300

    Frontend changed report page controller for fix menu links

--- assets/scripts/controllers/datas.js
+++ assets/scripts/controllers/datas.js
@@ -246,34 +246,3 @@ angular.module('heidaApp', ['ngDialog'])
 
 
   })
-  .controller('DataReportCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
-    Restangular.all('/api/data/').getList().then(function (datas) {
-      $scope.datas = datas;
-    });
-  })
-  .controller('DataReportDetailCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
-    Restangular.all('/api/data/' + $stateParams.department + '/' + $stateParams.indicator).getList().then(function (data) {
-      $scope.data = data;
-      console.log(data[0].department);
-      $scope.dept = $scope.data[0].department.name;
-      $scope.ind = $scope.data[0].indicator.name;
-      var lbl = [];
-      var dt = [];
-      for (var i = 0; i < $scope.data.length; i++) {
-        lbl.push($scope.data[i].year);
-        dt.push($scope.data[i].value);
-      }
-      $scope.bar = {
-
-        labels: lbl,
-        series: ['Series A'],
-
-        data: [
-          dt
-
-        ]
-
-      };
-    });
-
-});
