5083b81e28600b425614cb4f0b6a9bf6a3cedf0e
Berkant Eskicioglu <berkanteskicioglu@gmail.com>
2016-08-30 18:14:49 +0300

    Frontend fix router

--- assets/scripts/controllers/datas.js
+++ assets/scripts/controllers/datas.js
@@ -235,7 +235,7 @@ angular.module('heidaApp', ['ngDialog'])
         indicator: $scope.data.indicator.id,
         year: $scope.data.year,
         value: $scope.data.value,
-        answer: $scope.data.answers,
+        answer: $scope.data.answers
       };
       datas.post(dat);
       $state.go('dashboard.data', $stateParams, {
@@ -245,34 +245,35 @@ angular.module('heidaApp', ['ngDialog'])
     }
 
 
-  }).controller('DataReportCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
-  Restangular.all('/api/data/').getList().then(function (datas) {
-    $scope.datas = datas;
-  });
+  })
+  .controller('DataReportCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
+    Restangular.all('/api/data/').getList().then(function (datas) {
+      $scope.datas = datas;
+    });
+  })
+  .controller('DataReportDetailCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
+    Restangular.all('/api/data/' + $stateParams.department + '/' + $stateParams.indicator).getList().then(function (data) {
+      $scope.data = data;
+      console.log(data[0].department);
+      $scope.dept = $scope.data[0].department.name;
+      $scope.ind = $scope.data[0].indicator.name;
+      var lbl = [];
+      var dt = [];
+      for (var i = 0; i < $scope.data.length; i++) {
+        lbl.push($scope.data[i].year);
+        dt.push($scope.data[i].value);
+      }
+      $scope.bar = {
 
-}).controller('DataReportDetailCtrl', function ($scope, $http, Restangular, $state, $stateParams, $timeout) {
-  Restangular.all('/api/data/' + $stateParams.department + '/' + $stateParams.indicator).getList().then(function (data) {
-    $scope.data = data;
-    console.log(data[0].department);
-    $scope.dept = $scope.data[0].department.name;
-    $scope.ind = $scope.data[0].indicator.name;
-    var lbl = [];
-    var dt = [];
-    for (var i = 0; i < $scope.data.length; i++) {
-      lbl.push($scope.data[i].year)
-      dt.push($scope.data[i].value);
-    }
-    $scope.bar = {
+        labels: lbl,
+        series: ['Series A'],
 
-      labels: lbl,
-      series: ['Series A'],
+        data: [
+          dt
 
-      data: [
-        dt
+        ]
 
-      ]
-
-    };
-  });
+      };
+    });
 
 });
--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * Controller of the heidaApp
  */
 angular.module('heidaApp')
-  .controller('MainCtrl', function($scope,$position, Restangular) {
+  .controller('MainCtrl', function($scope, $position, Restangular) {
     // Get Users
     Restangular.all('/api/user').getList().then(function(users) {
       $scope.users = users;
@@ -18,7 +18,32 @@ angular.module('heidaApp')
       $scope.departments = departments;
     });
 
-    // Get Success Indicators but not yet
+    // Get Groups
+    Restangular.all('/api/group').getList().then(function (groups) {
+      $scope.groups = groups;
+    });
+
+    // Get Subgroups
+    Restangular.all('/api/subGroup').getList().then(function (subgroups) {
+      $scope.subgroups = subgroups;
+    });
+
+    // get Indicators
+    Restangular.all('/api/indicator').getList().then(function (indicators) {
+      $scope.indicators = indicators;
+    });
+
+    // Get Departments
+    Restangular.all('/api/department').getList().then(function (departments) {
+      $scope.departments = departments;
+    });
+
+    // Get Criterias
+    Restangular.all('/api/criteria').getList().then(function (criterias) {
+      $scope.criterias = criterias;
+    });
+
+    // Get Data
     Restangular.all('/api/data').getList().then(function (datas) {
       $scope.datas = datas;
     });
