1e774f1eac8fff33353f7008baeaacc9b93e3d94
Berkant Eskicioglu <berkanteskicioglu@gmail.com>
2016-09-03 02:37:05 +0300

    Frontend set users permission on pages

--- assets/scripts/directives/sidebar/sidebar.js
+++ assets/scripts/directives/sidebar/sidebar.js
@@ -19,17 +19,17 @@ angular.module('heidaApp')
         $scope.selectedMenu = 'dashboard';
         $scope.collapseVar = 0;
         $scope.multiCollapseVar = 0;
-        
+
         $scope.check = function(x){
-          
+
           if(x==$scope.collapseVar)
             $scope.collapseVar = 0;
           else
             $scope.collapseVar = x;
         };
-        
+
         $scope.multiCheck = function(y){
-          
+
           if(y==$scope.multiCollapseVar)
             $scope.multiCollapseVar = 0;
           else
