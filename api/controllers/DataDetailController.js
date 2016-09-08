5c3b55c09bc80eaeff6b1c5b2f3c8a4113b9328d
Zafer Taskent <zafer.taskent@gmail.com>
2016-09-08 16:38:19 -0400

    PR Code review fixes

--- api/controllers/DataDetailController.js
+++ api/controllers/DataDetailController.js
@@ -7,13 +7,13 @@
 
 module.exports = {
 	findAll: function (req, res) {
-		console.log("Layn1");
+
 		DataDetail.find().populate('criteria').populate('indicator').populate('question').exec(function (err, datadetails) {
 			res.json(datadetails);
 		});
 	},
 	find: function (req, res) {
-		console.log("Layn2");
+		
 		DataDetail.find().populate('criteria').populate('indicator').populate('question').exec(function (err, datadetails) {
 			res.json(datadetails);
 		});
--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -11,10 +11,8 @@ module.exports = function(req, res, next) {
   // Todo : Read Them from cache
   Authorization.findOne(req.path + ':' + req.method).exec(function(err, auth) {
     //if End point found check authorization
-    return next();
+
     if (auth) {
-      console.log(auth.role);
-      console.log(req.user.role);
       if (req.user.role >= auth.role) {
         return next();
       } else {
