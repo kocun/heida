be90c5c342d0d07f0d210d6e54edde7f6f7f287e
Adil Atılgan <aatilgan@ku.edu.tr>
2016-09-21 23:11:34 +0300

    Minor fixes

--- api/controllers/CriteriaController.js
+++ api/controllers/CriteriaController.js
@@ -8,7 +8,7 @@
 module.exports = {
 
   find: function (req, res) {
-    Criteria.find({sort :'name ASC'}).popoulate('questions').exec(function (err, criterias) {
+    Criteria.find({sort :'name ASC'}).populate('questions').exec(function (err, criterias) {
       res.json(criterias);
     });
   }
